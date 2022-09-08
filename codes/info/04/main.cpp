60317f54b5dde3dabb8f29ce01edd23d739f50f5
eric <fantasy_2003_@hotmail.com>
2022-09-08 09:58:30 +0800

    add simple01

--- codes/info/04/main.cpp
+++ codes/info/04/main.cpp
@@ -9,47 +9,28 @@
 
 int main(int argc, char** argv)
 {
-    //get all platforms (drivers)
+    // get all platforms (drivers), e.g. NVIDIA
     std::vector<cl::Platform> all_platforms;
     cl::Platform::get(&all_platforms);
-    if(all_platforms.size()==0){
+
+    if (all_platforms.size()==0) {
         std::cout<<" No platforms found. Check OpenCL installation!\n";
         exit(1);
     }
-    //// get all platforms (drivers), e.g. NVIDIA
-    //vector<cl::Platform> all_platforms;
-    //cl::Platform::get(&all_platforms);
+    cl::Platform default_platform=all_platforms[0];
+    std::cout << "Using platform: "<<default_platform.getInfo<CL_PLATFORM_NAME>()<<"\n";
 
-    //if (all_platforms.size()==0) {
-    //    std::cout<<" No platforms found. Check OpenCL installation!\n";
-    //    exit(1);
-    //}
+    // get default device (CPUs, GPUs) of the default platform
+    std::vector<cl::Device> all_devices;
+    default_platform.getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
+    if(all_devices.size()==0){
+        std::cout<<" No devices found. Check OpenCL installation!\n";
+        exit(1);
+    }
 
-    //cl_int CL_err = CL_SUCCESS;
-    //cl_uint numPlatforms = 0;
-
-    //CL_err = clGetPlatformIDs( 0, NULL, &numPlatforms );
-
-    //cl_platform_id * platforms = new cl_platform_id[ numPlatforms ];
-    //clGetPlatformIDs( numPlatforms, platforms, 0 );
-
-    //if ( CL_err == CL_SUCCESS )
-    //{
-    //    std::printf( "%u platform(s) found\n", numPlatforms );
-    //    for ( cl_uint i = 0; i < numPlatforms; ++ i )
-    //    {
-    //        std::size_t platform_name_length = 0;
-    //        clGetPlatformInfo( platforms[ i ], CL_PLATFORM_NAME, 0, 0, &platform_name_length );
-    //        char * platform_name = new char[ platform_name_length ];
-    //        clGetPlatformInfo( platforms[ i ], CL_PLATFORM_NAME, platform_name_length, platform_name, 0 );
-    //        std::printf(" [ %d ] %s\n", i, platform_name );
-
-    //    }
-    //}
-    //else
-    //{
-    //    std::printf( "clGetPlatformIDs(%i)\n", CL_err );
-    //}
+    // use device[1] because that's a GPU; device[0] is the CPU
+    cl::Device default_device=all_devices[0];
+    std::cout<< "Using device: "<<default_device.getInfo<CL_DEVICE_NAME>()<<"\n";
 
     return 0;
 }
